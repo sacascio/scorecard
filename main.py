c60a03fa9f15b3467f8b92e05494e294503334df
Salvatore Cascio <sacascio@cisco.com>
2020-08-10 14:22:48 -0400

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import scorecard
+#first cut August 10th
+
+def main():
+    baseball = scorecard.ScoreCard("football")
+    baseball.showSport()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
---
+++ scorecard.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+class ScoreCard:
+
+    def __init__(self,sport):
+        self.sport = sport
+
+    def showSport(self):
+        print (self.sport)
+
